f91df96be48602b4e82f37b3e43a1917b5c5686a
chengzhx76 <chengzhx76@qq.com>
2018-02-05 14:07:47 +0800

    mod

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import Vue from 'vue';
 import App from './App';
 
-import {router} from './router/index';
-import {appRouter} from './router/router';
+import router from './router';
 import store from './store';
-import 'iview/dist/styles/iview.css';
-import util from './utils/util';
 
+import 'iview/dist/styles/iview.css';
 import iView from 'iview';
 
 Vue.use(iView);
@@ -18,29 +16,5 @@ new Vue({
   router,
   store,
   template: '<App/>',
-  components: { App },
-  data: {
-    currentPageName: ''
-  },
-  mounted () {
-    this.currentPageName = this.$route.name;
-    // 显示打开的页面的列表
-    this.$store.commit('setOpenedList');
-    this.$store.commit('initCachepage');
-    // 权限菜单过滤相关
-    this.$store.commit('updateMenulist');
-    // iview-admin检查更新
-    util.checkUpdate(this);
-  },
-  created () {
-    let tagsList = [];
-    appRouter.map((item) => {
-      if (item.children.length <= 1) {
-        tagsList.push(item.children[0]);
-      } else {
-        tagsList.push(...item.children);
-      }
-    });
-    this.$store.commit('setTagsList', tagsList);
-  }
+  components: { App }
 });
